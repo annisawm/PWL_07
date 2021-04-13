1cb28f912e2ba197b6abfc77f5ae12c09bc12b05
annisawm <annisawm200601@gmail.com>
2021-04-13 19:54:11 +0700

    Prakt9_Make Controller Mahasiswa and Edit

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Mahasiswa;
+use App\Models\Kelas;
 use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
@@ -10,24 +11,34 @@ class MahasiswaController extends Controller
 
     public function index()
     {
-        $search = request()->query('search');
-        if($search) {
-            $posts = Mahasiswa::where('nama', 'LIKE', "%{$search}%")->paginate(5);
-        } else {
-            $posts = Mahasiswa::orderBy('nim', 'asc')->paginate(5);
-        }
+        // $search = request()->query('search');
+        // if($search) {
+        //     $posts = Mahasiswa::where('nama', 'LIKE', "%{$search}%")->paginate(5);
+        // } else {
+        //     $posts = Mahasiswa::orderBy('nim', 'asc')->paginate(5);
+        // }
         //fungsi eloquent menampilkan data menggunakan pagination
         // $mahasiswas = Mahasiswa::all(); // Mengambil semua isi tabel 
         // $posts = Mahasiswa::orderBy('nim', 'asc')->paginate(5); 
-        return view('users.index', compact('posts')); 
-        with('i', (request()->input('page', 1) - 1) * 5);
+        $mahasiswa = Mahasiswa::with('kelas')->get();
+        $paginate = Mahasiswa::orderBy('id_mahasiswa', 'asc')->paginate(3);
+        return view('users.index', ['mahasiswa' => $mahasiswa, 'paginate' => $paginate]); 
+        // return view('users.index', compact('posts')); 
+        // with('i', (request()->input('page', 1) - 1) * 5);
     }
 
+
+    // public function create()
+    // {
+    //     return view('users.create');
+    // }
 
     public function create()
     {
-        return view('users.create');
+        $kelas = Kelas::all(); //mendapatkan data dari tabel kelas
+        return view('users.create', ['kelas' => $kelas]);
     }
+
 
 
     public function store(Request $request)
@@ -36,7 +47,7 @@ class MahasiswaController extends Controller
         $request->validate([ 
             'nim' => 'required', 
             'nama' => 'required', 
-            'kelas' => 'required', 
+            'kelas_id' => 'required', 
             'jurusan' => 'required', 
             'email' => 'required', 
             'no_tlp' => 'required', 
@@ -44,11 +55,28 @@ class MahasiswaController extends Controller
             ]);
 
             //fungsi eloquent untuk menambah data
-            Mahasiswa::create($request->all());
+            // Mahasiswa::create($request->all());
+            
+            $mahasiswa = new Mahasiswa;
+            $mahasiswa->nim = $request->get('nim');
+            $mahasiswa->nama = $request->get('nama');
+            $mahasiswa->jurusan = $request->get('jurusan');
+            $mahasiswa->email = $request->get('email');
+            $mahasiswa->no_tlp = $request->get('no_tlp');
+            $mahasiswa->tgl_lahir = $request->get('tgl_lahir');
+            
+            $kelas = new Kelas;
+            $kelas->id = $request->get('kelas_id');
 
+             //fungsi eloquent untuk menambah data dengan relasi belongsTo
+            $mahasiswa->kelas()->associate($kelas);
+            $mahasiswa->save();
+            
             //jika data berhasil ditambahkan, akan kembali ke halaman utama
-            return redirect()->route('mahasiswa.index') 
-                ->with('success', 'Mahasiswa Berhasil Ditambahkan');
+            // return redirect()->route('mahasiswa.index')
+            // ->with('success', 'Mahasiswa Berhasil Ditambahkan');
+
+            dd($mahasiswa);
     }
 
     
