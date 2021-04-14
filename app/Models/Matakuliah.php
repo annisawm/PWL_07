68034bc5fbe739dd07b71835a324ad131b918d61
annisawm <annisawm200601@gmail.com>
2021-04-14 11:13:03 +0700

    Exercise Prakt9_Edit Models Matakuliah

---
+++ app/Models/Matakuliah.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Mahasiswa;
+
+class Matakuliah extends Model{
+    protected $table='matakuliah';//define this model is relate with table kelas
+    
+    public function mahasiswa(){
+        return $this->belongsToMany(Mahasiswa::class, 'mahasiswa_matakuliah', 'mahasiswa_id', 'matakuliah_id');
+    }
+}
