b9326b0487c87499746cd72cc4ded7a1b1dccef4
annisawm <annisawm200601@gmail.com>
2021-04-14 08:31:08 +0700

    Prakt9_edit mhs seeder

--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaSeeder extends Seeder
 {
@@ -13,6 +14,18 @@ class MahasiswaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $mhs = [
+            [
+                'nim' => '194234324',
+                'nama' => 'Anisa',
+                'kelas_id' => '1',
+                'jurusan' => 'Teknologi Informasi',
+                'no_tlp' => '08343534',
+                'tgl_lahir' => '08 September 2000',
+                'email' => 'anisa@gmail.com'
+            ]
+        ];
+
+        DB::table('mahasiswa')->insert($mhs);
     }
 }
