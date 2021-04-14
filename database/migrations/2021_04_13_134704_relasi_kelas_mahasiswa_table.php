ed37c910c6364b3e7a75fee87fcd9891a1bb6935
annisawm <annisawm200601@gmail.com>
2021-04-14 08:30:14 +0700

    prakt9_make migration kelas and relasi kelas mhs

--- database/migrations/2021_04_13_134704_relasi_kelas_mahasiswa_table.php
+++ database/migrations/2021_04_13_134704_relasi_kelas_mahasiswa_table.php
@@ -13,7 +13,7 @@ class RelasiKelasMahasiswaTable extends Migration
      */
     public function up()
     {
-        Schema::table('mahasiswa', function (Blueprint $table) {
+        Schema::table('mahasiswa', function(Blueprint $table) {
             $table->dropColumn('kelas'); //menghapus kolom kelas
             $table->unsignedBigInteger('kelas_id')->nullable(); //menambahkan kolom kelas_id
             $table->foreign('kelas_id')->references('id')->on('kelas'); //menambahkan foreign key di kolom kelas_id
@@ -27,10 +27,7 @@ class RelasiKelasMahasiswaTable extends Migration
      */
     public function down()
     {
-        Schema::table('mahasiswa', function (Blueprint $table) {
-            $table->string('kelas');
-            $table->dropForeign(['kelas_id']);
-            
-        });
+        $table->string('kelas');
+        $table->dropForeign(['kelas_id']);
     }
 }
